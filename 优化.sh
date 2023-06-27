0fae8916533e1c63ddb1dca988d442364b478cb5
Elon Cheng <995729579@qq.com>
2023-06-27 15:10:21 +0800

    Create 优化.sh

---
+++ 优化.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+namespace=kubesphere-logging-system
+
+function converting(){
+
+## Converting an existing configuration to a new one
+local resource=$1
+local kind=$2
+local list=$(kubectl get inputs.logging.kubesphere.io -A -o json)
+local name=($(echo $list | jq -r '.items[].metadata.name | @json'))
+local labels=($(echo $list | jq -r '.items[].metadata.labels | @json'))
+local spec=($(echo $list | jq -r '.items[].spec | @json'))
+local size=${#spec[*]}
+echo "Number of original input configuration files:$input_size"
+
+for((i=0;i<${size};i++));do
+cluster_resource__list[i]="{
+\"apiVersion\": \"fluentbit.fluent.io/v1alpha2\",
+\"kind\": \"${kind}\",
+\"metadata\": {
+\"name\": ${name[i]},
+\"labels\": ${labels[i]}
+},
+\"spec\": ${spec[i]}
+}"
+done
+}
+
+for((i=0;i<${size};i++));do
+echo ${cluster_resource_list[i]} | kubectl apply -f -
+done
+
+## Uninstall the fluentbit-operator and the original configuration
+for((i=0;i<${input_size};i++));do
+echo "${input_name[i]}"
+  input_temp=$(echo ${input_name[i]} | sed 's/"//g')
+  echo "$input_temp"
+   kubectl delete input.logging.kubesphere.io $input_temp -n ${namespace}
+done
+}
+
+
+converting(input ClusterInput)
+converting(parsers ClusterParser)
+converting(filters ClusterFilter)
+converting(outputs ClusterOutput)
+converting(fluentbitconfigs ClusterFluentBitConfig)
+
+fluentbit_list=$(kubectl get fluentbits.logging.kubesphere.io -A -o json)
+fluentbit_name=($(echo $fluentbit_list | jq -r '.items[].metadata.name'))
+fluentbit_spec=($(echo $fluentbit_list | jq -r '.items[].spec | @json'))
+fluentbit_labels=($(echo $fluentbit_list | jq -r '.items[].metadata.labels | @json'))
+fluentbit_size=${#fluentbit_name[*]}
+echo "Number of original fluentbit configuration files:$fluentbit_size"
+
+for((i=0;i<${fluentbit_size};i++));do
+cluster__fluentbit_list[i]="{
+\"apiVersion\": \"fluentbit.fluent.io/v1alpha2\",
+\"kind\": \"FluentBit\",
+\"metadata\": {
+\"name\":  \"fluent-operator-${fluentbit_name[i]}\",
+\"labels\": ${fluentbit_labels[i]},
+\"namespace\": \"${namespace}\"
+},
+\"spec\": ${fluentbit_spec[i]}
+}"
+done
+
+for((i=0;i<${fluentbit_size};i++));do
+echo ${cluster__fluentbit_list[i]} | kubectl apply -f -
+done
+
+# Determine if Deployment exists
+if kubectl get deployment -n $namespace $deployment >/dev/null 2>&1; then
+    # Delete Deployment if it exists
+    kubectl delete deployment -n $namespace $deployment
+    kubectl delete clusterrolebinding kubesphere:operator:fluentbit-operator
+    kubectl delete clusterrole kubesphere:operator:fluentbit-operator
+    kubectl delete serviceaccount fluentbit-operator -n $namespace
+    echo "Deployment $deployment deleted"
+else
+    # If it does not exist, output the message
+    echo "Deployment $deployment does not exist"
+fi
+
+## Delete the old crd
+kubectl get crd -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | grep "logging.kubesphere.io" | xargs -I crd_name kubectl delete crd crd_name
